108476e829409bfa9af452a3cce744b6acedfc37
Melissa Silva <32807254+melipass@users.noreply.github.com>
2022-03-12 14:14:19 -0300

    WIP: Layer functionality added. Separated normal sum from sum-range.
    Co-Authored-By: Alatrion <58146457+Alatrion@users.noreply.github.com>

--- task-1/node.js
+++ task-1/node.js
@@ -1,17 +1,37 @@
-const { time } = require('console');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+//app.use(express.json());
+app.use(bodyParser.urlencoded());
+app.listen(3000);
 
-let result = 0;
-app.post('/sum', function(req,res){
-    let starting_number = req.body.starting_number;
-    let ending_number = req.body.ending_number;
+app.post('/sum-range', function(req,res){
+    console.log(req.body);
+    let result = parseInt(0);
+    var starting_number;
+    var ending_number;
+    starting_number = parseInt(req.body.starting_number);
+    ending_number = parseInt(req.body.ending_number);
     for (let i = starting_number; i < ending_number; i++) {
         result = result + i;
-        
+    }
+    if (req.body.final === '1'){
+        result = result + ending_number;
     }
     res.send(result.toString());
 })
+
+app.post('/sum', function(req,res){
+    let result = parseInt(0);
+    var num1;
+    var num2;
+    num1 = parseInt(req.body.num1);
+    num2 = parseInt(req.body.num2);
+    result = num1 + num2;
+    res.send(result.toString());
+})
+
+
 
 // https://localhost:4000/sum?nums=1,2,3
